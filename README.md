b5c31dd2e5c5b88861a5772dc4ad85aa59b0b186
Peter Hu <peterbohai@gmail.com>
2020-06-18 19:29:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Random (Codewars) Problem Selector 
+
+This is a Python script for randomly selecting problems from a qustion bank. The selected problems are remembered and will not be be selected in the future (until all problems in the question bank is completed).
+
+## Installation
+
+Download the script along with the JSON file. The JSON file contains completed questions from different kyu levels on [Codewars](https://www.codewars.com/). Feel free to modify the file contents.
+
+## Usage
+
+```bash
+python3 random_problem.py
+```
+
+Note for easier/faster access, [create an alias](https://jonsuh.com/blog/bash-command-line-shortcuts/) for the above command.
+
+### Screenshots
+
+<img src="/screenshots/screenshot-1.png" width=50% />
+<br/>
+<img src="/screenshots/screenshot-2.png" width=50% />
+<br/>
+<img src="/screenshots/screenshot-3.png" width=50% />
+
+## Contributing
+
+If you experience any bugs or see anything that can be improved or added, please feel free to [open an issue](https://github.com/PeterBohai/bundo/issues) here or simply contact me on any of my socials (Email and [Linkedin](https://www.linkedin.com/in/peterhu08) can be found in the footer of any Bundo page). Thanks in advance!
---
+++ random_problem.py
@@ -0,0 +1,60 @@
+import random
+import json
+
+
+class Style:
+    RESET = "\033[0m"
+    RED = "\033[31m"
+    BLUE_D = "\033[34m"
+    BLUE_L = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[33m"
+    UNDERLINE = "\033[4m"
+    BOLD = "\033[1m"
+
+
+# formatting
+kyu_string = Style.BOLD + "kyu" + Style.RESET
+
+# load question bank
+with open("data_file.json", "r") as read_file:
+    data = json.load(read_file)
+    question_bank = data["question_bank"]
+    seen_bank = data["seen_bank"]
+
+print()
+command = input("Hi, would you like to SELECT(s) or ADD(a) a question or RESET(r)?  ")
+
+with open("data_file.json", "w") as write_file:
+
+    if command.strip() == "a" or command.strip() == "ADD":
+        adding = True
+        while adding:
+            question_title = input("What's the question title?  ")
+            kyu = input(f"Awesome! Now what's the {kyu_string} of this question?  ")
+            question_bank[kyu].append(question_title.strip())
+            adding = input("Do you want to add another question (y/n)?  ") == "y"
+            if adding:
+                print("---------------------------------------")
+
+    elif command.strip() == "s" or command.strip() == "SELECT":
+        kyu = input(f"Please indicate {kyu_string} level to practice:  ")
+        if question_bank[kyu]:
+            selected_question = random.choice(question_bank[kyu])
+            seen_bank[kyu].append(selected_question)
+            question_bank[kyu].remove(selected_question)
+            print(f"Try {Style.GREEN + selected_question + Style.RESET }\033[0m")
+
+        else:   # No more questions left in the questions list. Repopulate with all the seen questions.
+            question_bank[kyu] = seen_bank[kyu][:]
+            seen_bank[kyu].clear()
+            print("Already seen all questions. Please restart to loop through again.")
+
+    elif command.strip() == "r" or command.strip() == "RESET":
+        kyu = input(f"Please indicate which {kyu_string} questions to reset:  ")
+        question_bank[kyu].extend(seen_bank[kyu])
+        seen_bank[kyu].clear()
+        print(f"Successfully restarted kyu {kyu} questions.")
+
+    print()
+    json.dump(data, write_file)
